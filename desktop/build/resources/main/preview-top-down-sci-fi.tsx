ba18c2e002df9c4308ecbd28fb185d582568ea55
michaelsargsyan42 <48992476+michaelsargsyan42@users.noreply.github.com>
2022-05-13 23:26:56 +0400

    Add files via upload

---
+++ desktop/build/resources/main/preview-top-down-sci-fi.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="preview-top-down-sci-fi" tilewidth="16" tileheight="16" tilecount="198" columns="18">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="preview-top-down-sci-fi.png" width="288" height="176"/>
+</tileset>
